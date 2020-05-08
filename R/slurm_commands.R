e8055aece036ca443ccfe766a2c371a80df464df
Kovacs Mate <mate.kovacs@alphacruncher.com>
2020-05-08 15:13:11 +0200

    Doc and typo fix

--- R/slurm_commands.R
+++ R/slurm_commands.R
@@ -12,7 +12,7 @@ sbatch <- function(script, n_cpus=4, queue="intq", use_mpi=FALSE, user_name = NU
     dir.create("~/files/hpc_job_logs")
   }
   cluster_path <- read.delim("/lifecycle/.clusterpath", header = FALSE, stringsAsFactors = FALSE)[1,1]
-  if(is.null(user_name) {
+  if(is.null(user_name)) {
     user_name <- suppressWarnings({ read.delim("/secrets/username", header = FALSE, stringsAsFactors = FALSE)[1,1] })
   }
   aid <- read.delim('/lifecycle/.aid', header = FALSE, stringsAsFactors = FALSE)[1,1]
@@ -33,7 +33,7 @@ sbatch <- function(script, n_cpus=4, queue="intq", use_mpi=FALSE, user_name = NU
 #' @param user_name Username at the Nuvolos cluster, leave this empty from a Nuvolos application
 #' @export
 scancel <- function(job_id, user_name = NULL) {
-  if(is.null(user_name) {
+  if(is.null(user_name)) {
     user_name <- suppressWarnings({ read.delim("/secrets/username", header = FALSE, stringsAsFactors = FALSE)[1,1] })
   }
   system(sprintf("ssh -o ServerAliveInterval=30 %s@hpc.nuvolos.cloud \"module load slurm && scancel %s\"", user_name, job_id))
@@ -44,7 +44,7 @@ scancel <- function(job_id, user_name = NULL) {
 #' @param user_name Username at the Nuvolos cluster, leave this empty from a Nuvolos application
 #' @export
 squeue <- function(user_name = NULL) {
-  if(is.null(user_name) {
+  if(is.null(user_name)) {
     user_name <- suppressWarnings({ read.delim("/secrets/username", header = FALSE, stringsAsFactors = FALSE)[1,1] })
   }
   system(sprintf("ssh -o ServerAliveInterval=30 %s@hpc.nuvolos.cloud \"module load slurm && squeue\"", user_name))
@@ -53,14 +53,14 @@ squeue <- function(user_name = NULL) {
 #' Run job as an interactive R script
 #' @param script The R script to execute on the cluster. The path to the script should start ~/
 #' @param n_cpus The number of cpus requested for the operation
-#' @param queue The queue to use for the job. By default this is intq. 
+#' @param queue The queue to use for the job. By default this is intq.
 #' @param use_mpi Whether to use MPI for running the job (required for multi-node jobs)
 #' @param sync_wait The amount of time to wait (in seconds) after job completion for the system to synchronize files between Nuvolos and the high performance computing cluster. Larger files warrant larger wait times.
 #' @param report_freq The frequency of polling job information from the cluster (by default and at least 15 seconds)
 #' @param user_name Username at the Nuvolos cluster, leave this empty from a Nuvolos application
 #' @export
 run_job_interactive <- function(script, n_cpus = 4, queue = "intq", use_mpi=FALSE, sync_wait = 60, report_freq = 15, user_name = NULL) {
-  
+
   if (report_freq < 15) {
       stop("Error: Please provide a polling frequency of at least 15 seconds.")
   }
@@ -68,7 +68,7 @@ run_job_interactive <- function(script, n_cpus = 4, queue = "intq", use_mpi=FALS
   if (sync_wait < 60) {
       stop("Error: Please provide a synchronization period of at least 60 seconds. We suggest longer wait periods if you expect a large amount of information to be emitted by your cluster process.")
   }
-  
+
   val <- sbatch(script, n_cpus, queue, use_mpi, user_name)
   m <- regexec("Submitted batch job (\\d*)", val, perl=TRUE)
   job_id <- as.integer(regmatches(val, m)[[1]][2])
@@ -94,11 +94,11 @@ run_job_interactive <- function(script, n_cpus = 4, queue = "intq", use_mpi=FALS
 }
 
 #' Provide detailed information on status of running job
-#' @param job_id The job to be checked. 
+#' @param job_id The job to be checked.
 #' @param user_name Username at the Nuvolos cluster, leave this empty from a Nuvolos application
 #' @export
 slookup_job <- function(job_id, user_name) {
-  if(is.null(user_name) {
+  if(is.null(user_name)) {
     user_name <- suppressWarnings({ read.delim("/secrets/username", header = FALSE, stringsAsFactors = FALSE)[1,1] })
   }
   ret <- system(sprintf("ssh -o ServerAliveInterval=30 %s@hpc.nuvolos.cloud \"module load slurm && scontrol show job %s\"", user_name, job_id), intern = TRUE)
@@ -110,9 +110,9 @@ slookup_job <- function(job_id, user_name) {
 #' @export
 extract_state <- function(lookup_val) {
   if (is.na(lookup_val[4])) {
-    return(-1) # job info doesn't exist, it has been stopped some time ago  
+    return(-1) # job info doesn't exist, it has been stopped some time ago
   }
-  
+
   m <- regexec("JobState=(\\w*) Reason=.*", lookup_val[4], perl = TRUE)
   job_state <- regmatches(lookup_val[4], m)[[1]][2]
   return(job_state)
