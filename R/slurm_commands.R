9bd1cc4755970be21858872247dd36d156d0c448
Máté Kovács <mate.kovacs@alphacruncher.com>
2020-05-08 12:05:22 +0200

    Changing from scc-secondary@alphacruncher to hpc.nuvolos.cloud

--- R/slurm_commands.R
+++ R/slurm_commands.R
@@ -15,11 +15,11 @@ sbatch <- function(script, n_cpus=4, queue="intq", use_mpi=FALSE) {
   aid <- read.delim('/lifecycle/.aid', header = FALSE, stringsAsFactors = FALSE)[1,1]
   r_version <- paste0(R.version$major,".",R.version$minor)
   if (use_mpi) {
-    command_value <- sprintf("ssh -o ServerAliveInterval=30 %s@scc-secondary.alphacruncher.net \"module load slurm R/intel/mkl/%s && export R_LIBS_USER=%s/lib/%s HOME=%s && sbatch --export=ALL -p %s -n %s -o \\\"%s/files/hpc_job_logs/job-%%j.out\\\" -e \\\"%s/files/hpc_job_logs/job-%%j.err\\\" --wrap \\\"mpirun --quiet -np 1 Rscript --verbose %s\\\"\"",
+    command_value <- sprintf("ssh -o ServerAliveInterval=30 %s@hpc.nuvolos.cloud \"module load slurm R/intel/mkl/%s && export R_LIBS_USER=%s/lib/%s HOME=%s && sbatch --export=ALL -p %s -n %s -o \\\"%s/files/hpc_job_logs/job-%%j.out\\\" -e \\\"%s/files/hpc_job_logs/job-%%j.err\\\" --wrap \\\"mpirun --quiet -np 1 Rscript --verbose %s\\\"\"",
     user_name, r_version, cluster_path, aid, cluster_path,queue, n_cpus, cluster_path, cluster_path, script)
     system(command_value, intern = TRUE)
   } else {
-    command_value <- sprintf("ssh -o ServerAliveInterval=30 %s@scc-secondary.alphacruncher.net \"module load slurm R/intel/mkl/%s && export R_LIBS_USER=%s/lib/%s HOME=%s NUM_CPUS=%s && sbatch --export=ALL -p %s -n %s -o \\\"%s/files/hpc_job_logs/job-%%j.out\\\" -e \\\"%s/files/hpc_job_logs/job-%%j.err\\\" --wrap \\\"Rscript --verbose %s\\\"\"",
+    command_value <- sprintf("ssh -o ServerAliveInterval=30 %s@hpc.nuvolos.cloud \"module load slurm R/intel/mkl/%s && export R_LIBS_USER=%s/lib/%s HOME=%s NUM_CPUS=%s && sbatch --export=ALL -p %s -n %s -o \\\"%s/files/hpc_job_logs/job-%%j.out\\\" -e \\\"%s/files/hpc_job_logs/job-%%j.err\\\" --wrap \\\"Rscript --verbose %s\\\"\"",
     user_name, r_version, cluster_path, aid, cluster_path, n_cpus, queue, n_cpus, cluster_path, cluster_path, script)
     system(command_value, intern = TRUE)
   }
@@ -30,14 +30,14 @@ sbatch <- function(script, n_cpus=4, queue="intq", use_mpi=FALSE) {
 #' @export
 scancel <- function(job_id) {
   user_name <- suppressWarnings({ read.delim("/secrets/username", header = FALSE, stringsAsFactors = FALSE)[1,1] })
-  system(sprintf("ssh -o ServerAliveInterval=30 %s@scc-secondary.alphacruncher.net \"module load slurm && scancel %s\"", user_name, job_id))
+  system(sprintf("ssh -o ServerAliveInterval=30 %s@hpc.nuvolos.cloud \"module load slurm && scancel %s\"", user_name, job_id))
 }
 
 #' Get slurm job status
 #' @export
 squeue <- function() {
   user_name <- suppressWarnings({ read.delim("/secrets/username", header = FALSE, stringsAsFactors = FALSE)[1,1] })
-  system(sprintf("ssh -o ServerAliveInterval=30 %s@scc-secondary.alphacruncher.net \"module load slurm && squeue\"", user_name))
+  system(sprintf("ssh -o ServerAliveInterval=30 %s@hpc.nuvolos.cloud \"module load slurm && squeue\"", user_name))
 }
 
 #' Run job as an interactive R script
@@ -87,7 +87,7 @@ run_job_interactive <- function(script, n_cpus = 4, queue = "intq", use_mpi=FALS
 #' @export
 slookup_job <- function(job_id) {
   user_name <- suppressWarnings({ read.delim("/secrets/username", header = FALSE, stringsAsFactors = FALSE)[1,1] })
-  ret <- system(sprintf("ssh -o ServerAliveInterval=30 %s@scc-secondary.alphacruncher.net \"module load slurm && scontrol show job %s\"", user_name, job_id), intern = TRUE)
+  ret <- system(sprintf("ssh -o ServerAliveInterval=30 %s@hpc.nuvolos.cloud \"module load slurm && scontrol show job %s\"", user_name, job_id), intern = TRUE)
   return(ret)
 }
 
