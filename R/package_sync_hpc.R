36276e2f0d72488ac102fbf972d26ed60585db56
Kovacs Mate <mate.kovacs@alphacruncher.com>
2020-08-31 10:59:43 +0200

    cd ~/files fix - export home before

--- R/package_sync_hpc.R
+++ R/package_sync_hpc.R
@@ -1,6 +1,6 @@
 #' Synchronize packages between Nuvolos and the HPC Cluster
 #' @export
-package_sync_hpc <- function() {
+package_sync_hpc <- function(use_gsl=FALSE) {
   installed_packages_local <- dir("/dhlib")
   cluster_path <- read.delim("/lifecycle/.clusterpath", header = FALSE, stringsAsFactors = FALSE)[1,1]
   aid <- read.delim('/lifecycle/.aoid', header = FALSE, stringsAsFactors = FALSE)[1,1]
@@ -23,7 +23,8 @@ package_sync_hpc <- function() {
     r_version <- paste0(R.version$major,".",R.version$minor)
     for (p in packages_to_install) {
       print(sprintf("Installing package: %s", p))
-      system(sprintf("ssh -o ServerAliveInterval=30 %s@hpc.nuvolos.cloud \"cd ~/files && module load R/intel/mkl/%s && Rscript -e \\\"install.packages('%s',lib='%s/lib/%s', repos='%s')\\\"\" && mkdir -p ~/hpc_installed/lib/%s/%s",user_name, r_version, p, cluster_path, aid ,options()$repos,aid, p))
+      system(sprintf("ssh -o ServerAliveInterval=30 %s@hpc.nuvolos.cloud \"export HOME=%s && cd ~/files && module load R/intel/mkl/%s && Rscript -e \\\"install.packages('%s',lib='%s/lib/%s', repos='%s')\\\"\" && mkdir -p ~/hpc_installed/lib/%s/%s",
+                     user_name, cluster_path, r_version, p, cluster_path, aid ,options()$repos,aid, p))
     }
   } else {
     print("No additional packages to install.")
@@ -33,7 +34,7 @@ package_sync_hpc <- function() {
 #' Install packages locally and remotely
 #' @param package A vector of package names
 #' @export
-install.packages <- function(package) {
+install.packages <- function(package, use_gsl = FALSE) {
   cluster_path <- read.delim("/lifecycle/.clusterpath", header = FALSE, stringsAsFactors = FALSE)[1,1]
   aid <- read.delim('/lifecycle/.aoid', header = FALSE, stringsAsFactors = FALSE)[1,1]
   user_name <- suppressWarnings({ read.delim("/secrets/username", header = FALSE, stringsAsFactors = FALSE)[1,1] })
@@ -50,7 +51,8 @@ install.packages <- function(package) {
   for (p in package) {
     r_version <- paste0(R.version$major,".",R.version$minor)
     print(sprintf("Installing package on cluster: %s", p))
-    system(sprintf("ssh -o ServerAliveInterval=30 %s@hpc.nuvolos.cloud \"cd ~/files && module load R/intel/mkl/%s && Rscript -e \\\"install.packages('%s',lib='%s/lib/%s', repos='%s')\\\"\" && mkdir -p ~/hpc_installed/lib/%s/%s",user_name, r_version, p, cluster_path, aid, options()$repos, aid,p))
+    system(sprintf("ssh -o ServerAliveInterval=30 %s@hpc.nuvolos.cloud \"export HOME=%s && cd ~/files && module load R/intel/mkl/%s && Rscript -e \\\"install.packages('%s',lib='%s/lib/%s', repos='%s')\\\"\" && mkdir -p ~/hpc_installed/lib/%s/%s",
+                   user_name, cluster_path, r_version, p, cluster_path, aid, options()$repos, aid,p))
   }
 
   utils::install.packages(package)
@@ -60,7 +62,7 @@ install.packages <- function(package) {
 #' Install github packages locally and remotely
 #' @param repo A github repo URL
 #' @export
-install_github <- function(repo) {
+install_github <- function(repo, use_gsl = FALSE) {
   cluster_path <- read.delim("/lifecycle/.clusterpath", header = FALSE, stringsAsFactors = FALSE)[1,1]
   aid <- read.delim('/lifecycle/.aoid', header = FALSE, stringsAsFactors = FALSE)[1,1]
   user_name <- suppressWarnings({ read.delim("/secrets/username", header = FALSE, stringsAsFactors = FALSE)[1,1] })
@@ -73,8 +75,8 @@ install_github <- function(repo) {
   # remove any lock folders
   system(sprintf("ssh %s@hpc.nuvolos.cloud 'rm -rf %s/lib/%s/00*'",user_name, cluster_path, aid))
 
-  system(sprintf("ssh -o ServerAliveInterval=30 %s@hpc.nuvolos.cloud \"cd ~/files && module load R/intel/mkl/%s && export R_LIBS_USER=%s/lib/%s && Rscript -e \\\"remotes::install_github('%s')\\\"\" && mkdir -p ~/hpc_installed/lib/%s/%s",
-                user_name, r_version, cluster_path, aid, repo, aid, tail(strsplit(repo,"/")[[1]],1)))
+  system(sprintf("ssh -o ServerAliveInterval=30 %s@hpc.nuvolos.cloud \"export HOME=%s && cd ~/files && module load R/intel/mkl/%s && export R_LIBS_USER=%s/lib/%s && Rscript -e \\\"remotes::install_github('%s')\\\"\" && mkdir -p ~/hpc_installed/lib/%s/%s",
+                user_name, cluster_path, r_version, cluster_path, aid, repo, aid, tail(strsplit(repo,"/")[[1]],1)))
   remotes::install_github(repo)
 }
 
@@ -82,7 +84,7 @@ install_github <- function(repo) {
 #' Synchronize packages between Nuvolos and the HPC Cluster
 #' @param path A local filesystem path (must be given in relative terms, starting with '~/')
 #' @export
-install_local <- function(path) {
+install_local <- function(path, use_gsl = FALSE) {
   gsub("(.*)/$","\\1",path, perl=TRUE)
   if (!grepl("^~/",path)) {
     stop("Error: path must start with ~/")
@@ -99,7 +101,7 @@ install_local <- function(path) {
   # remove any lock folders
   system(sprintf("ssh %s@hpc.nuvolos.cloud 'rm -rf %s/lib/%s/00*'",user_name, cluster_path, aid))
 
-  system(sprintf("ssh -o ServerAliveInterval=30 %s@hpc.nuvolos.cloud \"cd ~/files && module load R/intel/mkl/%s && export R_LIBS_USER=%s/lib/%s HOME=%s && Rscript -e \\\"remotes::install_local('%s',force=TRUE)\\\"\" && mkdir -p ~/hpc_installed/lib/%s/%s",
-                user_name, r_version, cluster_path, aid, cluster_path, path, aid, tail(strsplit(path,"/")[[1]],1)))
+  system(sprintf("ssh -o ServerAliveInterval=30 %s@hpc.nuvolos.cloud \"export HOME=%s && cd ~/files && module load R/intel/mkl/%s && export R_LIBS_USER=%s/lib/%s HOME=%s && Rscript -e \\\"remotes::install_local('%s',force=TRUE)\\\"\" && mkdir -p ~/hpc_installed/lib/%s/%s",
+                user_name, cluster_path, r_version, cluster_path, aid, cluster_path, path, aid, tail(strsplit(path,"/")[[1]],1)))
   remotes::install_local(path=path, force=TRUE)
 }
